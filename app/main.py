e223454a13d3d8dacc507567028f2613b120a5a6
simon <si.sibomana@gmail.com>
2026-04-07 14:47:21 +0200

    send correlation id

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ import socket
 
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    client, addr = server.accept()
+    resp_msg_size = int('0').to_bytes(4, byteorder='big')
+    resp_msg_header = int('7').to_bytes(4, byteorder='big')
+    resp = resp_msg_size + resp_msg_header
+    client.send(resp)
+    client.close()
+
 
 
 if __name__ == "__main__":
