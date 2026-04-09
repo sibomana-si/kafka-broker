cb03b2cf33771ac3c24d90c14d7ad7361c05735f
simon <sibo200@gmail.com>
2026-04-09 14:26:51 +0200

    parse api version

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ def main():
         client_request = client.recv(1024)
         resp_msg_size = int('0').to_bytes(4, byteorder='big')
         correlation_id = client_request[8:12]
-        resp = resp_msg_size + correlation_id
+        request_api_version = int.from_bytes(client_request[6:8], byteorder='big')
+        if request_api_version in (0,1,2,3,4):
+            error_code = int('0').to_bytes(2, byteorder='big')
+        else:
+            error_code = int('35').to_bytes(2, byteorder='big')
+        resp = resp_msg_size + correlation_id + error_code
         client.sendall(resp)
 
 
