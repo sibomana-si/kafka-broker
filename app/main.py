7098c863126a8a159f44b6f2ab0ee0c532ed9e6a
simon <sibo200@gmail.com>
2026-04-10 16:46:13 +0200

    handle ApiVersions requests

--- app/main.py
+++ app/main.py
@@ -2,21 +2,30 @@ import socket
 
 
 def main():
-    with (socket.create_server(("localhost", 9092), reuse_port=True)
-          as server_socket):
+    with (socket.create_server(("localhost", 9092), reuse_port=True) as server_socket):
         client, addr = server_socket.accept()
         print(f"Accepted connection from {addr}")
         client_request = client.recv(1024)
-        resp_msg_size = int('0').to_bytes(4, byteorder='big')
         correlation_id = client_request[8:12]
         request_api_version = int.from_bytes(client_request[6:8], byteorder='big')
         if request_api_version in (0,1,2,3,4):
-            error_code = int('0').to_bytes(2, byteorder='big')
+            error_code = int(0).to_bytes(2, byteorder='big')
+            api_key_array_length = int(2).to_bytes(1, byteorder='big')
+            api_key = int('18').to_bytes(2, byteorder='big')
+            api_key_min_version = int(0).to_bytes(2, byteorder='big')
+            api_key_max_version = int(4).to_bytes(2, byteorder='big')
+            api_key_tag_buffer = int(0).to_bytes(1, byteorder='big')
+            throttle_time = int(0).to_bytes(4, byteorder='big')
+            throttle_time_tag_buffer = int(0).to_bytes(1, byteorder='big')
+            resp_body = error_code + api_key_array_length + api_key + api_key_min_version + api_key_max_version \
+                        + api_key_tag_buffer + throttle_time + throttle_time_tag_buffer
         else:
-            error_code = int('35').to_bytes(2, byteorder='big')
-        resp = resp_msg_size + correlation_id + error_code
+            error_code = int(35).to_bytes(2, byteorder='big')
+            resp_body = error_code
+        msg_size = len(correlation_id) + len(resp_body)
+        resp_msg_size = int(msg_size).to_bytes(4, byteorder='big')
+        resp = resp_msg_size + correlation_id + resp_body
         client.sendall(resp)
-
 
 
 
